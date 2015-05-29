1b1051630f0aae77b301e1460ef308a3681523da
CraigKuriger <apprentice@StMac5.local>
2015-05-29 15:48:08 +1200

    need to sort images

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,29 @@ Category.create(name: "Music")
 Category.create(name: "Books")
 Category.create(name: "Other")
 
-Category.all.each do |category|
-  4.times do
-    category.products.create(name: Faker::Lorem.word.capitalize, description: Faker::Lorem.paragraph(2), image_url: Faker::Avatar.image, price: Money.new(9999, "NZD"))
-  end
-end
+# Category.all.each do |category|
+#   4.times do
+#     category.products.create(name: Faker::Lorem.word.capitalize, description: Faker::Lorem.paragraph(2), image_url: Faker::Avatar.image, price: Money.new(9999, "NZD"))
+#   end
+# end
 
-movies = Category.find(name: "Movies")
-movies.products.create(name: "G", description: "f", price: "20.00")
+movies = Category.find_by(name: "Movies")
+music = Category.find_by(name: "Music")
+books = Category.find_by(name: "Books")
+other = Category.find_by(name: "Other")
+movies.products.create(name: "Grumpy Cat's Worst Christmas Ever", description: "Worst. Movie. Ever.", image_url: "http://upload.wikimedia.org/wikipedia/en/1/1d/Grumpy_Cat's_Worst_Christmas_Ever_cover.jpg", price: "29.99")
+
+music.products.create(name: "Grumpy Cat's Worst Christmas Ever Soundtrack", description: "Worst. Soundtrack. Ever.", image_url: "http://ecx.images-amazon.com/images/I/81MCRODBhfL._SL1500_.jpg", price: "29.99")
+
+books.products.create(name: "Grumpy Cat: A Grumpy Book", description: "Worst. Book. Ever.", image_url: "http://ecx.images-amazon.com/images/I/51SszqxmPyL._SY344_BO1,204,203,200_.jpg", price: "29.99")
+
+books.products.create(name: "Grumpy Cat's Guide To Life", description: "Worst. Book. Ever.", image_url: "https://s-media-cache-ak0.pinimg.com/236x/11/7d/4d/117d4d2eb0b80e31ede36806fa043b53.jpg", price: "29.99")
+
+books.products.create(name: "Grumpy Cat: 2016 Calendar", description: "Worst. Calendar. Ever.", image_url: "http://ecx.images-amazon.com/images/I/617wmIWgfKL._SX258_BO1,204,203,200_.jpg", price: "29.99")
+
+other.products.create(name: "Grumpy Cat Plush Toy", description: "You can't be half a Gangster", image_url: "http://ecx.images-amazon.com/images/I/51NNW9ZbHaL._SY300_.jpg", price: "29.99")
+
+
 
 User.create(username: "Craig", firstname: "Craig", lastname: "Kuriger", email: "craig@craig.com", password: "qwerty123", admin: true)
 
